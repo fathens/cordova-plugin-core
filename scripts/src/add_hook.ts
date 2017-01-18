8cbd975f9664efb3890984aee6e7c273e62fdfa8
Kunio SAWATANI <fsawa@fathens.org>
2017-01-18 01:26:58 +0000

    ディレクトリ .hooks の下にファイルを書き込むだけの実装

--- scripts/src/add_hook.ts
+++ scripts/src/add_hook.ts
@@ -3,36 +3,15 @@ import * as fs from 'async-file';
 
 console.log(`Working on ${process.cwd()}`);
 
-const script_line = process.argv[2];
-const script_name = process.argv[3] || 'prestart';
-console.log(`Adding scripts['${script_name}'] <- '${script_line}'`);
+const hook_name = process.argv[2];
+const hook_code = process.argv[3];
+console.log(`Adding scripts['${hook_name}'] <- '${hook_code}'`);
 
-main("../../../package.json");
+main("../../../nole_modules/.hooks");
 
-async function main(target_file) {
-    if (await fs.exists(target_file)) {
-        const result = await modify(await fs.readFile(target_file, 'utf-8'));
-        if (result) {
-            await fs.writeFile(target_file, result, 'utf-8');
-            console.log(`Wrote to '${target_file}'`);
-        } else {
-            console.log(`No need to modify '${target_file}'`);
-        }
-    } else {
-        console.log(`No target: '${target_file}'`);
-    }
-}
-
-async function modify(data) {
-    const json = JSON.parse(data);
-
-    if (!json.scripts) json.scripts = {};
-
-    const line = json.scripts[script_name];
-    if (line) {
-        json.scripts[script_name] = line + '; ' + script_line;
-    } else {
-        json.scripts[script_name] = script_line;
-    }
-    return JSON.stringify(json, null, 2);
+async function main(target_dir) {
+    if (!(await fs.exists(target_dir))) await fs.mkdir(target_dir);
+    const target_file = [target_dir, hook_name].join('/');
+    await fs.writeFile(target_file, hook_code, 'utf-8');
+    await fs.chmod(target_file, 0o755);
 }
